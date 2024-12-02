3d279de5ae445bec218c67cb0925280bae0be7d4
OttoElomaa <otto.elomaa99@gmail.com>
2024-12-02 16:04:47 +0200

    Fixed repo name in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/{repo-name}/',
+  base: '/hh-frontend-project-deploy/',
   plugins: [react()],
   test: {
     globals: true,
